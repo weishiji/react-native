00753db75f27ca60ca2cbb5cf100831cb9129672
lxg <lxg1986@gmail.com>
2016-06-15 16:29:13 +0800

    >     at Parser.pp.parseStatement (/Users/lxg/Documents/react-native-android/node_modules/babylon/lib

--- src/scenes/app.js
+++ src/scenes/app.js
@@ -1,11 +1,24 @@
 /* @flow */
 
 import React from 'react'
+import {BackAndroid,ToastAndroid,} from 'react-native';
 import { Actions, Scene } from 'react-native-router-flux'
 import { styles } from '@components/NavigationBar'
 import LauchContainer from '@containers/LauchContainer'
 import CounterContainer from '@containers/CounterContainer'
 
+
+BackAndroid.addEventListener('hardwareBackPress', function() {
+	console.log(Actions,'this is actions')
+	try {
+		Actions.pop();
+       return true;
+   }
+   catch (err) {
+       ToastAndroid.show("Cannot pop. Exiting the app...", ToastAndroid.SHORT);
+       return true;
+   }
+});
 const scenes = Actions.create(
   <Scene key="app" navigationBarStyle={styles.container}>
     <Scene key="welcome" component={LauchContainer} title="Welcome" />
